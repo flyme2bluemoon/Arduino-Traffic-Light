5332c60ad532b94a17801d56555e3882a86a300f
Matthew Shen <37197710+flyme2bluemoon@users.noreply.github.com>
2020-06-11 23:31:29 -0400

    Added traffic light logic

--- trafficlight.ino
+++ trafficlight.ino
@@ -1,12 +1,15 @@
 #define RED1 8
 #define YELLOW1 9
 #define GREEN1 10
+#define BUTTON1 7
+
 #define RED2 11
 #define YELLOW2 12
 #define GREEN2 13
+#define BUTTON2 6
 
 void setup() {
-  // Set pins to output
+    // Set pins to output
     pinMode(RED1, OUTPUT);
     pinMode(YELLOW1, OUTPUT);
     pinMode(GREEN1, OUTPUT);
@@ -14,14 +17,64 @@ void setup() {
     pinMode(YELLOW2, OUTPUT);
     pinMode(GREEN2, OUTPUT);
 
-  // Set pins to input
+    // Set pins to input
+    pinMode(BUTTON1, INPUT);
+    pinMode(BUTTON2, INPUT);
+
+    return;
 }
 
-void main () {
+void loop () {
+    // Start up sequence
+    delay(1000);
+    
     digitalWrite(RED1, HIGH);
     digitalWrite(RED2, HIGH);
     digitalWrite(YELLOW1, HIGH);
     digitalWrite(YELLOW2, HIGH);
     digitalWrite(GREEN1, HIGH);
     digitalWrite(GREEN2, HIGH);
+
+    delay(1000);
+
+    // The actual thing
+
+    digitalWrite(RED1, HIGH);
+    digitalWrite(RED2, HIGH);
+    digitalWrite(YELLOW1, LOW);
+    digitalWrite(YELLOW2, LOW);
+    digitalWrite(GREEN1, LOW);
+    digitalWrite(GREEN2, LOW);
+
+    delay(2000);
+
+    digitalWrite(RED1, LOW);
+    digitalWrite(GREEN1, HIGH);
+
+    delay(7000);
+
+    digitalWrite(GREEN1, LOW);
+    digitalWrite(YELLOW1, HIGH);
+
+    delay(3000);
+
+    digitalWrite(YELLOW1, LOW);
+    digitalWrite(RED1, HIGH);
+
+    delay(2000);
+
+    digitalWrite(RED2, LOW);
+    digitalWrite(GREEN2, HIGH);
+
+    delay(7000);
+
+    digitalWrite(GREEN2, LOW);
+    digitalWrite(YELLOW2, HIGH);
+
+    delay(3000);
+
+    digitalWrite(YELLOW2, LOW);
+    digitalWrite(RED2, HIGH);
+
+    return;
 }
